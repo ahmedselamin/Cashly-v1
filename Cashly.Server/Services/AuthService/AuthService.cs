5327573e9e271fd8d848f1315cf222573b0c7385
eddyiiz <eddyxsayf@gmail.com>
2024-07-10 09:53:31 +0200

    updated create token method an updated token

--- Cashly.Server/Services/AuthService/AuthService.cs
+++ Cashly.Server/Services/AuthService/AuthService.cs
@@ -122,13 +122,14 @@ public class AuthService : IAuthService
         var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
                 .GetBytes(_config.GetSection("AppSettings:Token").Value));
 
+
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
         var token = new JwtSecurityToken
             (
-                claims: claims,
-                expires: DateTime.Now.AddDays(1),
-                signingCredentials: creds
+              claims: claims,
+              expires: DateTime.Now.AddDays(1),
+              signingCredentials: creds
             );
 
         var jwt = new JwtSecurityTokenHandler().WriteToken(token);
