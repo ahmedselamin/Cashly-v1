fc4c0135f4c0fe120b4ee96cd262447758f96fa0
eddyiiz <eddyxsayf@gmail.com>
2024-07-10 10:45:29 +0200

    ChangePassword endpoint

--- Cashly.Server/Controllers/AuthController.cs
+++ Cashly.Server/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Cashly.Server.Controllers
 {
@@ -35,6 +37,20 @@ namespace Cashly.Server.Controllers
         public async Task<ActionResult<ServiceResponse<string>>> Login(UserLogin request)
         {
             var result = await _authService.Login(request.Username, request.Password);
+
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("change-password"), Authorize]
+        public async Task<ActionResult<ServiceResponse<bool>>> ChangePassword([FromBody] string newPassword)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var result = await _authService.ChangePassword(int.Parse(userId), newPassword);
 
             if (!result.Success)
             {
