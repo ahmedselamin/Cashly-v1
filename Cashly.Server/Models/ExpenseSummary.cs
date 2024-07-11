67f527c2f8623050c323c52ce7d77ee841d46bc4
eddyiiz <eddyxsayf@gmail.com>
2024-07-11 20:52:59 +0200

    expense model

---
+++ Cashly.Server/Models/ExpenseSummary.cs
@@ -0,0 +1,9 @@
+﻿namespace Cashly.Server.Models;
+
+public class ExpenseSummary
+{
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public decimal TotalAmount { get; set; }
+}
