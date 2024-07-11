8994e289d8e512f6be5de15ca40ea81dca14939c
eddyiiz <eddyxsayf@gmail.com>
2024-07-11 21:37:39 +0200

    refined

--- Cashly.Server/Services/ReportService/IReportService.cs
+++ Cashly.Server/Services/ReportService/IReportService.cs
@@ -1,6 +1,8 @@
-﻿namespace Cashly.Server.Services.ReportService
+﻿namespace Cashly.Server.Services.ReportService;
+
+public interface IReportService
 {
-    public interface IReportService
-    {
-    }
+    Task<ServiceResponse<decimal>> GetTotalIncome(DateTime startDate, DateTime endDate);
+    Task<ServiceResponse<decimal>> GetTotalExpenditure(DateTime startDate, DateTime endDate);
+    Task<ServiceResponse<decimal>> GetTotalExpenses(DateTime startDate, DateTime endDate);
 }
